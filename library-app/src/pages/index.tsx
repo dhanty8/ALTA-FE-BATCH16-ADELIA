6a359ae2d90e423da2e5a67987b69659b3a78e0b
dhanty8 <adeliadhanti1208@gmail.com>
2023-11-08 14:55:42 +0700

    create app structures for library app

---
+++ library-app/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/layout"
+
+const Index = () => {
+    return (
+        <div>
+           <Layout/>
+            <p className="text-red-500 text-9xl fond-bold">HELLO WORLD</p>
+        </div>
+    )
+}
+
+export default Index 
---
+++ library-app/src/utils/main.tsx
@@ -0,0 +1,11 @@
+import '../styles/index.css'
+
+import Index from '../pages'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Index/>
+  </React.StrictMode>,
+)
